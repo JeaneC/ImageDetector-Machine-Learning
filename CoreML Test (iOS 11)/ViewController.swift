3a8c9e7e12aa0afc301cd759ab39bc1179521048
Jeane Carlos <volbohel@gmail.com>
2017-08-10 13:26:21 -0400

    Initial Commit

---
+++ CoreML Test (iOS 11)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreML Test (iOS 11)
+//
+//  Created by Jeane Carlos on 8/10/17.
+//  Copyright © 2017 jisy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
